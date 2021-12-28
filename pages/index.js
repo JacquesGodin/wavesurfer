51aa4be086a2671417f14bbef9894248da1a2389
Tiago Godinho <tiagogodinho@MacBook-Air.lan>
2021-12-28 19:51:14 +0000

    added wave customization controls

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
 import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
+import React from 'react';
+import { TwitterPicker } from 'react-color';
+//import { imageToBase64, base64ToImage } from "base64-2-img";
+
+
+// Create the component color picker
+class Component extends React.Component {
+  handleChangeComplete(color, event) {
+  }
+
+  render() {
+    return <TwitterPicker onChange={ this.handleChangeComplete }/>;
+  }
+}
 
 const formWaveSurferOptions = (ref) => ({
   container: ref,
@@ -19,8 +33,48 @@ export default function IndexPage() {
   const wavesurfer = useRef(null);
   const [playing, setPlaying] = useState(false);
 
+// predefined URL. The ideal is to only load the waveform once the user has input a link
   const url =
     "https://www.mfiles.co.uk/mp3-downloads/brahms-st-anthony-chorale-theme-two-pianos.mp3";
+
+
+// Upload the link to the song's location
+const submitLink = async (event) => {
+    event.preventDefault();
+    const link = event.target.link.value;
+    const radius = event.target.radius.value;
+    const width = event.target.width.value;
+    const color = event.target.color.value;
+
+    const formWaveSurferOptions = (ref) => ({
+      container: ref,
+      waveColor: color,
+      progressColor: "#0178FF",
+      cursorColor: "OrangeRed",
+      barWidth: width,
+      barRadius: radius,
+      responsive: true,
+      height: 500,
+      normalize: true,
+      partialRender: true
+    });
+
+    console.log(color)
+
+    // make variable url to be equals to the provided link
+    const url = link
+
+    const WaveSurfer = (await import("wavesurfer.js")).default;
+
+    // destroy the current displayed waveform
+    wavesurfer.current.destroy();
+
+    // get the options again
+    const options = formWaveSurferOptions(waveformRef.current);
+    wavesurfer.current = WaveSurfer.create(options);
+    wavesurfer.current.load(url)
+  };
+
 
   useEffect(() => {
     create();
@@ -37,7 +91,6 @@ export default function IndexPage() {
 
     const options = formWaveSurferOptions(waveformRef.current);
     wavesurfer.current = WaveSurfer.create(options);
-
     wavesurfer.current.load(url);
   };
 
@@ -46,13 +99,49 @@ export default function IndexPage() {
     wavesurfer.current.playPause();
   };
 
+  const display = () => {
+  }
+
+  const download = () => {
+    
+    console.log(wavesurfer.current.exportImage('image/png',1))
+    console.log("Saved")
+
+
+    var image = new Image();
+    image.src = wavesurfer.current.exportImage('image/png',1);
+    document.body.appendChild(image);
+
+  
+  }
+
+
+
   return (
+    
     <div>
-      <div id="waveform" ref={waveformRef} />
+      
+    <div id="waveform" ref={waveformRef} />
+    <form className='flex flex-col' onSubmit={submitLink}>
+	    
+      <label htmlFor='link' className='mb-2 italic'>Link</label>
+	    <input className='mb-4 border-b-2' id='link' name='link' type='text' autoComplete='link' required />
+      
+      <label htmlFor='radius' className='mb-2 italic'>Radius</label>
+	    <input className='mb-4 border-b-2' id='radius' name='radius' type='number' autoComplete='radius' required />
+      
+      <label htmlFor='width' className='mb-2 italic'>Witdh</label>
+	    <input className='mb-4 border-b-2' id='width' name='width' type='number' autoComplete='width' required />
+      
+      {/* <TwitterPicker htmlFor='color' className='mb-2 italic'>Color </TwitterPicker> */}
+      <input className='mb-5 border-b-2' id='color' name='color' type='color' autoComplete='color'/>
+      
+      <button type='submit' className='px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700'>Submit</button>
+    </form>
       <div className="controls">
         <div onClick={handlePlayPause}>{!playing ? "Playing" : "Pause"}</div>
       </div>
-      <Link href={{ pathname: "/about" }}>xxx</Link>
+      <div onClick={download}>{"Download"}</div>
     </div>
   );
 }
--- pages/some.js
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function SomePage() {
-  return (
-    <div>
-      <Link href={{ pathname: "/about" }}>xxx</Link>
-    </div>
-  );
-}
